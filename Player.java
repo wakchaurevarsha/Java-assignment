0b97188f2d941033119a85c40b1178330e6fc9be
wakchaurevarsha <41702366+wakchaurevarsha@users.noreply.github.com>
2020-10-17 18:25:44 +0530

    ASSIGNMENT DAY 5

---
+++ Player.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Player{
+	      Scanner sc = new Scanner(System.in);
+		  
+		  String name;
+		  int score=0;
+		  
+		  
+		  public void getDetails(){
+			  System.out.println("Enter PLayer name");
+			  name=sc.next();
+		  }
+}
+
+
+
+
