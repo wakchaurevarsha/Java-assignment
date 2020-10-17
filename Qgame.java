3f31f023c4010023459a2bbeed72f826927a5c47
wakchaurevarsha <41702366+wakchaurevarsha@users.noreply.github.com>
2020-10-17 18:23:25 +0530

    ASSIGNMENT DAY 5

---
+++ Qgame.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.util.Scanner;
+
+public class Qgame{
+	public static void main(String[]args){
+		
+		//Quiz application
+		
+		Game game= new Game();
+		game.initGame();
+		game.play();
+		
+	}
+}
